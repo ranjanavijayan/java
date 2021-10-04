dc6f53196f25f9186c4b8e92874926df48d9b269
ranjanavijayan <77263930+ranjanavijayan@users.noreply.github.com>
2021-10-04 22:52:24 +0530

    Add files via upload

---
+++ Lab Cycle 2/TCP.java
@@ -0,0 +1,30 @@
+/*Aim:Client server communication using Socket – TCP/IP
+java Server side code:*/
+import java.net.*; 
+import java.io.*;
+class TCPServer
+ {
+  public static void main(String args[])throws Exception
+  {
+   String msg;
+   ServerSocket ss=new ServerSocket(1095);
+  Socket s=ss.accept();
+  System.out.println(" is connected"+s.isConnected());
+  System.out.println(" s="+s.getInetAddress());
+  System.out.println(" port"+s.getPort());
+  DataOutputStream dos =new DataOutputStream(s.getOutputStream());
+  DataInputStream in=new DataInputStream(System.in);
+  System.out.println(" Enter a message for client");
+  while(true)
+  { 
+   String str=in.readLine();
+   if ((str==null || str.equals("end")))
+    {
+     break;
+    }
+   else {
+    dos.writeUTF(str);
+     } 
+  s.close();
+ }
+}
